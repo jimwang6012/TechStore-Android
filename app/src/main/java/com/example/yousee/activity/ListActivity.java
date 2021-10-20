df5047ee6dbb6310961f80506573bdd8fb5d48c9
nalydnix <dylanxin@outlook.com>
2021-10-21 11:15:35 +1300

    Revert "[fix] recycler item load animation"
    This reverts commit eaf6b803444a76d435017748f5c9a221f2aa64c7.

--- app/src/main/java/com/example/yousee/activity/ListActivity.java
+++ app/src/main/java/com/example/yousee/activity/ListActivity.java
@@ -35,6 +35,7 @@ public class ListActivity extends AppCompatActivity {
         RecyclerView items;
         EditText searchField;
         CoordinatorLayout coordinatorLayout;
+        NestedScrollView nestedScrollView;
         TextView message;
 
         public ViewHolder() {
@@ -44,6 +45,7 @@ public class ListActivity extends AppCompatActivity {
             items = (RecyclerView) findViewById(R.id.rv_items);
             searchField = (EditText) findViewById(R.id.search_button);
             coordinatorLayout = (CoordinatorLayout) findViewById(R.id.cl_list);
+            nestedScrollView = (NestedScrollView) findViewById(R.id.nsv_items);
             message = (TextView) findViewById(R.id.text_message);
         }
     }
